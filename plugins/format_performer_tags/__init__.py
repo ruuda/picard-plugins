9375941b2ee82463755986703bb30d5c6de1b6fc
Bob Swift <bswift@rsds.ca>
2018-10-16 21:46:37 -0600

    Update format_performer_tags to use 4 user defined sections

--- plugins/format_performer_tags/__init__.py
+++ plugins/format_performer_tags/__init__.py
@@ -27,10 +27,12 @@ each of the tracks.  The format of the resulting tags can be customized
 in the option settings page.
 '''
 
-PLUGIN_VERSION = "0.2"
+PLUGIN_VERSION = "0.05"
 PLUGIN_API_VERSIONS = ["2.0"]
 PLUGIN_LICENSE = "GPL-2.0 or later"
 PLUGIN_LICENSE_URL = "https://www.gnu.org/licenses/gpl-2.0.html"
+
+DEV_TESTING = False
 
 import re
 from picard import config, log
@@ -41,7 +43,7 @@ from picard.plugins.format_performer_tags.ui_options_format_performer_tags impor
 
 performers_split = re.compile(r", | and ").split
 
-WORD_LIST = ['guest', 'solo', 'additional', 'minor', 'lead', 'background']
+WORD_LIST = ['guest', 'solo', 'additional']
 
 def format_performer_tags(album, metadata, *args):
     word_dict = {}
@@ -51,59 +53,87 @@ def format_performer_tags(album, metadata, *args):
         mainkey, subkey = key.split(':', 1)
         if not subkey:
             continue
+        log.debug("%s: Formatting Performer [%s: %s]", PLUGIN_NAME, subkey, values,)
         instruments = performers_split(subkey)
-        log.debug("%s: Formatting Performer [%s]",
-                  PLUGIN_NAME,
-                  subkey,
-                  )
-        groups = { 1: [], 2: [], 3: [], }
+        for instrument in instruments:
+            if DEV_TESTING:
+                log.debug("%s: instrument (first pass): '%s'", PLUGIN_NAME, instrument,)
+            if instrument in WORD_LIST:
+                instruments[0] = "{0} {1}".format(instruments[0], instrument,)
+                instruments.remove(instrument)
+        groups = { 1: [], 2: [], 3: [], 4: [], }
         words = instruments[0].split()
         for word in words[:]:
-            if not word in WORD_LIST:
-                break
-            groups[word_dict[word]].append(word)
-            words.remove(word)
+            if word in WORD_LIST:
+                groups[word_dict[word]].append(word)
+                words.remove(word)
         instruments[0] = " ".join(words)
         display_group = {}
-        for group_number in range(1, 4):
+        for group_number in range(1, 5):
             if groups[group_number]:
-                log.debug("%s: groups[%s]: %s", PLUGIN_NAME, group_number, groups[group_number],)
-                groups[group_number].sort(reverse=config.setting["format_group_{0}_sort_desc".format(group_number)])
-                display_group[group_number] = config.setting["format_group_{0}_start_char".format(group_number)] + " ".join(groups[group_number]) + config.setting["format_group_{0}_end_char".format(group_number)]
-                display_group[group_number] = display_group[group_number] + " " if group_number < 2 else " " + display_group[group_number]
+                if DEV_TESTING:
+                    log.debug("%s: groups[%s]: %s", PLUGIN_NAME, group_number, groups[group_number],)
+                group_separator = config.setting["format_group_{0}_sep_char".format(group_number)]
+                if not group_separator:
+                    group_separator = " "
+                display_group[group_number] = config.setting["format_group_{0}_start_char".format(group_number)] + group_separator.join(groups[group_number]) + config.setting["format_group_{0}_end_char".format(group_number)]
+                #display_group[group_number] = display_group[group_number] + " " if group_number < 3 else " " + display_group[group_number]
             else:
                 display_group[group_number] = ""
-        log.debug("%s: display_group: %s", PLUGIN_NAME, display_group,)
+        if DEV_TESTING:
+            log.debug("%s: display_group: %s", PLUGIN_NAME, display_group,)
         del metadata[key]
         for instrument in instruments:
-            newkey = '%s:%s%s%s' % (mainkey, display_group[1], instrument, display_group[2])
+            if DEV_TESTING:
+                log.debug("%s: instrument (second pass): '%s'", PLUGIN_NAME, instrument,)
+            words = instrument.split()
+            if (len(words) > 1) and (words[-1] in ["vocal", "vocals",]):
+                vocals = " ".join(words[:-1])
+                instrument = words[-1]
+            else:
+                vocals = ""
+            if vocals:
+                group_number = config.setting["format_group_vocals"]
+                temp_group = groups[group_number][:]
+                if group_number < 2:
+                    temp_group.append(vocals)
+                else:
+                    temp_group.insert(0, vocals)
+                group_separator = config.setting["format_group_{0}_sep_char".format(group_number)]
+                if not group_separator:
+                    group_separator = " "
+                display_group[group_number] = config.setting["format_group_{0}_start_char".format(group_number)] + group_separator.join(temp_group) + config.setting["format_group_{0}_end_char".format(group_number)]
+                #display_group[group_number] = display_group[group_number] + " " if group_number < 3 else " " + display_group[group_number]
+
+            newkey = ('%s:%s%s%s%s' % (mainkey, display_group[1], instrument, display_group[2], display_group[3],)).strip()
             log.debug("%s: newkey: %s", PLUGIN_NAME, newkey,)
             for value in values:
-                metadata.add_unique(newkey, value + display_group[3])
+                metadata.add_unique(newkey, (value + display_group[4]).strip())
 
 
 class FormatPerformerTagsOptionsPage(OptionsPage):
 
     NAME = "format_performer_tags"
-    TITLE = "Format Performer Tagss"
+    TITLE = "Format Performer Tags"
     PARENT = "plugins"
 
     options = [
-        config.IntOption("setting", "format_group_additional", 1),
-        config.IntOption("setting", "format_group_guest", 1),
-        config.IntOption("setting", "format_group_minor", 1),
-        config.IntOption("setting", "format_group_solo", 1),
-        config.IntOption("setting", "format_group_lead", 1),
-        config.IntOption("setting", "format_group_background", 1),
+        config.IntOption("setting", "format_group_additional", 3),
+        config.IntOption("setting", "format_group_guest", 4),
+        config.IntOption("setting", "format_group_solo", 3),
+        config.IntOption("setting", "format_group_vocals", 2),
         config.TextOption("setting", "format_group_1_start_char", ''),
         config.TextOption("setting", "format_group_1_end_char", ''),
-        config.BoolOption("setting", "format_group_1_sort_desc", False),
-        config.TextOption("setting", "format_group_2_start_char", '('),
-        config.TextOption("setting", "format_group_2_end_char", ')'),
-        config.BoolOption("setting", "format_group_2_sort_desc", False),
-        config.TextOption("setting", "format_group_3_start_char", '('),
+        config.TextOption("setting", "format_group_1_sep_char", ''),
+        config.TextOption("setting", "format_group_2_start_char", ', '),
+        config.TextOption("setting", "format_group_2_end_char", ''),
+        config.TextOption("setting", "format_group_2_sep_char", ''),
+        config.TextOption("setting", "format_group_3_start_char", ' ('),
         config.TextOption("setting", "format_group_3_end_char", ')'),
-        config.BoolOption("setting", "format_group_3_sort_desc", False),
+        config.TextOption("setting", "format_group_3_sep_char", ''),
+        config.TextOption("setting", "format_group_4_start_char", ' ('),
+        config.TextOption("setting", "format_group_4_end_char", ')'),
+        config.TextOption("setting", "format_group_4_sep_char", ''),
     ]
 
     def __init__(self, parent=None):
@@ -112,38 +142,122 @@ class FormatPerformerTagsOptionsPage(OptionsPage):
         self.ui.setupUi(self)
 
     def load(self):
-        self.ui.format_group_additional.setValue(config.setting["format_group_additional"])
-        self.ui.format_group_guest.setValue(config.setting["format_group_guest"])
-        self.ui.format_group_minor.setValue(config.setting["format_group_minor"])
-        self.ui.format_group_solo.setValue(config.setting["format_group_solo"])
-        self.ui.format_group_lead.setValue(config.setting["format_group_lead"])
-        self.ui.format_group_background.setValue(config.setting["format_group_background"])
+        # Enable external link
+        self.ui.format_description.setOpenExternalLinks(True)
+
+        # Settings for Keyword: additional
+        temp = config.setting["format_group_additional"]
+        if temp > 3:
+            self.ui.additional_rb_4.setChecked(True)
+        elif temp > 2:
+            self.ui.additional_rb_3.setChecked(True)
+        elif temp > 1:
+            self.ui.additional_rb_2.setChecked(True)
+        else:
+            self.ui.additional_rb_1.setChecked(True)
+
+        # Settings for Keyword: guest
+        temp = config.setting["format_group_guest"]
+        if temp > 3:
+            self.ui.guest_rb_4.setChecked(True)
+        elif temp > 2:
+            self.ui.guest_rb_3.setChecked(True)
+        elif temp > 1:
+            self.ui.guest_rb_2.setChecked(True)
+        else:
+            self.ui.guest_rb_1.setChecked(True)
+
+        # Settings for Keyword: solo
+        temp = config.setting["format_group_solo"]
+        if temp > 3:
+            self.ui.solo_rb_4.setChecked(True)
+        elif temp > 2:
+            self.ui.solo_rb_3.setChecked(True)
+        elif temp > 1:
+            self.ui.solo_rb_2.setChecked(True)
+        else:
+            self.ui.solo_rb_1.setChecked(True)
+
+        # Settings for all vocal keywords
+        temp = config.setting["format_group_vocals"]
+        if temp > 3:
+            self.ui.vocals_rb_4.setChecked(True)
+        elif temp > 2:
+            self.ui.vocals_rb_3.setChecked(True)
+        elif temp > 1:
+            self.ui.vocals_rb_2.setChecked(True)
+        else:
+            self.ui.vocals_rb_1.setChecked(True)
+
+        # Settings for word group 1
         self.ui.format_group_1_start_char.setText(config.setting["format_group_1_start_char"])
         self.ui.format_group_1_end_char.setText(config.setting["format_group_1_end_char"])
-        self.ui.format_group_1_sort_desc.setChecked(config.setting["format_group_1_sort_desc"])
+        self.ui.format_group_1_sep_char.setText(config.setting["format_group_1_sep_char"])
+
+        # Settings for word group 2
         self.ui.format_group_2_start_char.setText(config.setting["format_group_2_start_char"])
         self.ui.format_group_2_end_char.setText(config.setting["format_group_2_end_char"])
-        self.ui.format_group_2_sort_desc.setChecked(config.setting["format_group_2_sort_desc"])
+        self.ui.format_group_2_sep_char.setText(config.setting["format_group_2_sep_char"])
+
+        # Settings for word group 3
         self.ui.format_group_3_start_char.setText(config.setting["format_group_3_start_char"])
         self.ui.format_group_3_end_char.setText(config.setting["format_group_3_end_char"])
-        self.ui.format_group_3_sort_desc.setChecked(config.setting["format_group_3_sort_desc"])
+        self.ui.format_group_3_sep_char.setText(config.setting["format_group_3_sep_char"])
+
+        # Settings for word group 4
+        self.ui.format_group_4_start_char.setText(config.setting["format_group_4_start_char"])
+        self.ui.format_group_4_end_char.setText(config.setting["format_group_4_end_char"])
+        self.ui.format_group_4_sep_char.setText(config.setting["format_group_4_sep_char"])
+
 
     def save(self):
-        config.setting["format_group_additional"] = self.ui.format_group_additional.value()
-        config.setting["format_group_guest"] = self.ui.format_group_guest.value()
-        config.setting["format_group_minor"] = self.ui.format_group_minor.value()
-        config.setting["format_group_solo"] = self.ui.format_group_solo.value()
-        config.setting["format_group_lead"] = self.ui.format_group_lead.value()
-        config.setting["format_group_background"] = self.ui.format_group_background.value()
+        # Process 'additional' keyword settings
+        temp = 1
+        if self.ui.additional_rb_2.isChecked(): temp = 2
+        if self.ui.additional_rb_3.isChecked(): temp = 3
+        if self.ui.additional_rb_4.isChecked(): temp = 4
+        config.setting["format_group_additional"] = temp
+
+        # Process 'guest' keyword settings
+        temp = 1
+        if self.ui.guest_rb_2.isChecked(): temp = 2
+        if self.ui.guest_rb_3.isChecked(): temp = 3
+        if self.ui.guest_rb_4.isChecked(): temp = 4
+        config.setting["format_group_guest"] = temp
+
+        # Process 'solo' keyword settings
+        temp = 1
+        if self.ui.solo_rb_2.isChecked(): temp = 2
+        if self.ui.solo_rb_3.isChecked(): temp = 3
+        if self.ui.solo_rb_4.isChecked(): temp = 4
+        config.setting["format_group_solo"] = temp
+
+        # Process all vocal keyword settings
+        temp = 1
+        if self.ui.vocals_rb_2.isChecked(): temp = 2
+        if self.ui.vocals_rb_3.isChecked(): temp = 3
+        if self.ui.vocals_rb_4.isChecked(): temp = 4
+        config.setting["format_group_vocals"] = temp
+
+        # Settings for word group 1
         config.setting["format_group_1_start_char"] = self.ui.format_group_1_start_char.text()
         config.setting["format_group_1_end_char"] = self.ui.format_group_1_end_char.text()
-        config.setting["format_group_1_sort_desc"] = self.ui.format_group_1_sort_desc.isChecked()
+        config.setting["format_group_1_sep_char"] = self.ui.format_group_1_sep_char.text()
+
+        # Settings for word group 2
         config.setting["format_group_2_start_char"] = self.ui.format_group_2_start_char.text()
         config.setting["format_group_2_end_char"] = self.ui.format_group_2_end_char.text()
-        config.setting["format_group_2_sort_desc"] = self.ui.format_group_2_sort_desc.isChecked()
+        config.setting["format_group_2_sep_char"] = self.ui.format_group_2_sep_char.text()
+
+        # Settings for word group 3
         config.setting["format_group_3_start_char"] = self.ui.format_group_3_start_char.text()
         config.setting["format_group_3_end_char"] = self.ui.format_group_3_end_char.text()
-        config.setting["format_group_3_sort_desc"] = self.ui.format_group_3_sort_desc.isChecked()
+        config.setting["format_group_3_sep_char"] = self.ui.format_group_3_sep_char.text()
+
+        # Settings for word group 4
+        config.setting["format_group_4_start_char"] = self.ui.format_group_4_start_char.text()
+        config.setting["format_group_4_end_char"] = self.ui.format_group_4_end_char.text()
+        config.setting["format_group_4_sep_char"] = self.ui.format_group_4_sep_char.text()
 
 
 # Register the plugin to run at a HIGH priority.
